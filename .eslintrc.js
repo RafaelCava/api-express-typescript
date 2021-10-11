6dc41ff5a511fecbe9297f6a4a5a2b954ec4c6d6
Rafael cavalcante <ra.facavalcante@hotmail.com>
2021-10-11 20:29:48 -0300

    src

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,6 @@ module.exports = {
     sourceType: 'module'
   },
   rules: {
+    'array-callback-return': 'off'
   }
 }
--- src/controllers/Usuarios/UsersControllers.ts
+++ src/controllers/Usuarios/UsersControllers.ts
@@ -6,16 +6,19 @@ import bcrypt from 'bcrypt'
 class UserController {
   public async index (req: Request, res: Response): Promise<Response> {
     const users = await Users.find()
+    users.map(user => {
+      user.password = null
+    })
     return res.status(200).json(users)
   }
 
   public async createUser (req: Request, res: Response): Promise<Response> {
     try {
-      const { password, ...user } = req.body
-      const hash = bcrypt.hash(password, 10)
-      user.password = hash
-      const users = await Users.create(user)
-      return res.status(201).json(users)
+      const hash = await bcrypt.hash(req.body.password, 10)
+      req.body.password = hash
+      const users = await Users.create(req.body)
+      const { password, ...user } = users._doc
+      return res.status(201).json(user)
     } catch (error) {
       return res.status(400).json(error)
     }
