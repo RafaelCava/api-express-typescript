824d75c1cb51581fd0b77589fd535aa208916c20
Rafael cavalcante <ra.facavalcante@hotmail.com>
2021-10-11 20:14:29 -0300

    controllers/Usuarios/UsersControllers:

--- src/controllers/Usuarios/UsersControllers.ts
+++ src/controllers/Usuarios/UsersControllers.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express'
 
 import Users from '../../schemas/UserSchema'
+
+import bcrypt from 'bcrypt'
 class UserController {
   public async index (req: Request, res: Response): Promise<Response> {
     const users = await Users.find()
@@ -9,7 +11,10 @@ class UserController {
 
   public async createUser (req: Request, res: Response): Promise<Response> {
     try {
-      const users = await Users.create(req.body)
+      const { password, ...user } = req.body
+      const hash = bcrypt.hash(password, 10)
+      user.password = hash
+      const users = await Users.create(user)
       return res.status(201).json(users)
     } catch (error) {
       return res.status(400).json(error)
