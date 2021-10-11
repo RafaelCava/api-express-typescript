1ffa871d0d39794d2eee519f34f7460b3308e265
Rafael <rafael.cavalcante@tallos.com.br>
2021-10-11 16:35:13 -0300

    iniciando projeto api express crud node usando typescript

---
+++ src/App.ts
@@ -0,0 +1,32 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+
+class App {
+  public express: express.Application
+
+  public constructor () {
+    this.express = express()
+    this.middlewares()
+    this.routes()
+    this.database()
+  }
+
+  private middlewares (): void {
+    this.express.use(express.json())
+    this.express.use(cors())
+  }
+
+  private database (): void {
+    mongoose.connect('mongodb://localhost:27017/tsnode')
+  }
+
+  private routes (): void {
+    this.express.route('/usuarios')
+      .get((req, res) => {
+        res.send('usuarios')
+      })
+  }
+}
+
+export default new App().express
---
+++ src/server.ts
@@ -0,0 +1,4 @@
+import app from './App'
+
+const port = 3333
+app.listen(port)
