f9bcb7931975b6de6f782cf33a9d8a874534a22c
Rafael Cavalcante <ra.facavalcante@hotmail.com>
2021-10-11 23:46:19 -0300

    removendo dist/

--- dist/server.js
+++
@@ -1,4 +0,0 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _App = require('./App'); var _App2 = _interopRequireDefault(_App);
-
-const port = 3333
-_App2.default.listen(port)
