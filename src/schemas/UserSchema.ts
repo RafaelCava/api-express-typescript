64e6a6986ed580fa8006ad3b40664ffea654427a
Rafael cavalcante <ra.facavalcante@hotmail.com>
2021-10-11 21:15:41 -0300

    schemas/UserSchema:

--- src/schemas/UserSchema.ts
+++ src/schemas/UserSchema.ts
@@ -1,6 +1,6 @@
-import { Schema, model } from 'mongoose'
+import { Schema, model, Document } from 'mongoose'
 
-interface UserInterface {
+interface UserInterface extends Document {
   email?: string,
   firstName?: string,
   lastName?: string,
