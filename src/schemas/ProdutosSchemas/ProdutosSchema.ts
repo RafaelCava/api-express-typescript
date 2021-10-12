03fcaeaeceaa0e5bfd3b7abef9ea8fdbcd4ed006
Rafael cavalcante <ra.facavalcante@hotmail.com>
2021-10-12 00:32:33 -0300

    schemas/ProdutosSchemas/ProdutosSchema:

--- src/schemas/ProdutosSchemas/ProdutosSchema.ts
+++ src/schemas/ProdutosSchemas/ProdutosSchema.ts
@@ -2,6 +2,7 @@ import { Schema, model, Document } from 'mongoose'
 
 interface ProdutosInterface extends Document {
   nome?: string,
+  idCliente?: string,
   quantidade?: number,
   preco?: number,
   descricao?: string
@@ -9,6 +10,7 @@ interface ProdutosInterface extends Document {
 
 const ProdutoSchema = new Schema({
   nome: String,
+  idCliente: String,
   quantidade: Number,
   preco: Number,
   descricao: String
