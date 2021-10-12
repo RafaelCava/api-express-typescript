28d839c07d7d6a12c89481be618b066ffd3dc69c
Rafael cavalcante <ra.facavalcante@hotmail.com>
2021-10-12 00:56:56 -0300

    controllers/Produtos/Produtos:

--- src/controllers/Produtos/Produtos.ts
+++ src/controllers/Produtos/Produtos.ts
@@ -20,23 +20,32 @@ class ProdutosControllers {
   }
 
   public async createProduto (req: Request, res: Response): Promise<Response> {
-    const { nome, idCliente, quantidade, preco, descricao } = req.body
-    if (!nome) {
-      return res.status(400).json({ message: 'Falta o campo nome' })
+    const { nome, quantidade, preco, descricao } = req.body
+    const authorization = req.headers.authorization
+    if (!authorization) {
+      return res.status(403).json({ message: 'Falta o token' })
     }
-    if (!idCliente) {
-      return res.status(403).json({ message: 'Falta o campo de id do cliente' })
-    }
-    if (!quantidade) {
-      return res.status(400).json({ message: 'Falta o campo de quantidade' })
-    }
-    if (!preco) {
-      return res.status(400).json({ message: 'Falta o campo de preço' })
-    }
-    if (!descricao) {
-      return res.status(400).json({ message: 'Falta o campo de descricao' })
-    }
+    const bearer = authorization.split(' ')
+    const [, token] = bearer
     try {
+      const user = jwt.verify(token, secureToken)
+      const { id: idCliente } = user
+      if (!nome) {
+        return res.status(400).json({ message: 'Falta o campo nome' })
+      }
+      if (!idCliente) {
+        return res.status(403).json({ message: 'Falta o campo de id do cliente' })
+      }
+      if (!quantidade) {
+        return res.status(400).json({ message: 'Falta o campo de quantidade' })
+      }
+      if (!preco) {
+        return res.status(400).json({ message: 'Falta o campo de preço' })
+      }
+      if (!descricao) {
+        return res.status(400).json({ message: 'Falta o campo de descricao' })
+      }
+      req.body.idCliente = idCliente
       await ProdutosSchema.create(req.body)
       return res.status(201).json({ message: 'usuário cadastrado com sucesso' })
     } catch (error) {
