247a748f3f5702aee6596a16c93f9a2ec90f47c2
rafael <ra.facavalcante@hotmail.com>
2021-11-28 14:07:17 -0300

    update: alterando schemas das oportunidades  - inserido propriedades dentro do scheam das oportunidades para que possa ser inserido nome da oportunidade e do vendedor

--- src/schemas/OportunidadesSchemas/OportunidadesSchema.ts
+++ src/schemas/OportunidadesSchemas/OportunidadesSchema.ts
@@ -3,11 +3,15 @@ import { Schema, model, Document } from 'mongoose'
 interface OportunidadeInterface extends Document {
   oportunidadeId: string
   vendedorId: string
+  vendedorNome: string
+  oportunidadeNome: string
 }
 
 const OportunidadeSchema = new Schema({
   oportunidadeId: String,
-  vendedorId: String
+  vendedorId: String,
+  vendedorNome: String,
+  oportunidadeNome: String
 }, {
   timestamps: true
 })
